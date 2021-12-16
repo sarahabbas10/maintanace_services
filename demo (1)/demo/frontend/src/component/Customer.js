e1b2f9794443c5ad64195c084f637000ee2f437a
sarahabbas10 <sarahabbas1418@gmail.com>
2021-12-16 08:30:28 +0300

    add cutomer component

---
+++ demo (1)/demo/frontend/src/component/Customer.js
@@ -0,0 +1,139 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+import { addCustomer } from '../reducers/customer/action'
+
+import { useSelector, useDispatch } from 'react-redux';
+import './customer.css'
+function Customer() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    // const [customer, setCustomer] = useState({});
+    let  phoneNo
+   let name
+   let password
+    let address 
+    const state = useSelector((state) => {
+       
+        return {
+            customer: state.customerReducer.customer,
+        }
+    })
+
+    const go = () => {
+        navigate("/myRequests");
+    }
+
+
+    const addReview = () => {
+        console.log("yehgv");
+    }
+
+
+    const newRequest = () => {
+        navigate("/new_request");
+    }
+
+    const changeName = (e) => {
+      name=e.target.value
+    }
+
+    const changeAddress = (e) => {
+        address=e.target.value;
+    }
+
+    const changePhoneNo = (e) => {
+        phoneNo=e.target.value;
+    }
+
+
+    const updateInfo = () => {
+       password=state.customer.password;
+       if (name) {
+        console.log("name: " + name);
+    } else name= state.customer.name;
+        if (address) {
+            console.log("address: " + address);
+        } else address= state.customer.address;
+        if (phoneNo) {
+            console.log("phoneNo : " + phoneNo);
+        } else phoneNo=state.customer.phoneNo;
+       update()
+
+
+    }
+
+    const update = () => {
+
+        console.log(name);
+        console.log(address);
+        console.log(phoneNo);
+        console.log(password);
+
+       
+       let  customer = { name, phoneNo, password, address }
+        console.log(customer);
+        axios
+        .put(`http://localhost:8080/customer/` + state.customer.idCustomer, customer)
+        .then((response) => {
+         
+            const action = addCustomer(response.data)
+            dispatch(action)
+            navigate("/")
+        })
+        .catch((error) =>
+            console.log(error));
+        
+    }
+
+
+    const putCustomer = (customer)=>{
+        if(name && address & phoneNo && password){
+       
+    }
+    }
+
+    return (
+
+        <>
+        {state.customer.idCustomer ?
+            <div class="container rounded bg-white mt-5 mb-5" id="counter">
+                <div class="row">
+
+
+                    <div class="col-md-5 border-right">
+                        <div class="p-3 py-5">
+                            <div class="d-flex justify-content-between align-items-center mb-3">
+                                <h4 class="text-right">Profile Settings</h4>
+                            </div>
+                            <div class="row mt-2">
+                                <div class="col-md-6"><label class="labels">Name</label>
+                                    <input type="text" class="form-control" placeholder={state.customer.name} onChange={changeName} />
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-12"><label class="labels">Mobile Number</label>
+                                    <input type="text" class="form-control" placeholder={state.customer.phoneNo} onChange={changePhoneNo} />
+                                </div>
+                                <div class="col-md-12"><label class="labels">Address</label>
+                                    <input type="text" class="form-control" placeholder={state.customer.address} onChange={changeAddress} />
+                                </div>
+                            </div>
+
+                            <div class="mt-5 text-center">
+                                <button class="btn btn-primary profile-button" type="button" onClick={updateInfo}>Save Profile</button>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+
+:  navigate("/")}
+        </>
+    )
+}
+export default Customer;
+
