7221969e9d06cfc44759a89f20b460245119bd84
sarahabbas10 <sarahabbas1418@gmail.com>
2021-12-16 08:41:04 +0300

    add reducers to index.jsfile

--- demo (1)/demo/frontend/src/index.js
+++ demo (1)/demo/frontend/src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
-
+import {Provider} from 'react-redux';
+import store from './reducers/store.js';
 ReactDOM.render(
   <React.StrictMode>
+     <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
